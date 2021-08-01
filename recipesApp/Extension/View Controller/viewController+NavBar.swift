90ddd79e9cb5f4a40debf88d9f919dfae267d11e
Ahmed elbasha <64855329+AhmedElbasha97@users.noreply.github.com>
2021-07-31 20:28:41 -0700

    update the extension of navbar

--- recipesApp/Extension/View Controller/viewController+NavBar.swift
+++ recipesApp/Extension/View Controller/viewController+NavBar.swift
@@ -9,13 +9,50 @@
 import Foundation
 import UIKit
 extension UIViewController{
-    func setupNavController(title: String, view: UIView) {
-           self.navigationItem.title = title
-           self.navigationController?.navigationBar.clipsToBounds = true
-           self.navigationController?.navigationBar.layer.maskedCorners = [.layerMinXMinYCorner,.layerMaxXMinYCorner]
-           self.navigationController?.navigationBar.titleTextAttributes = [
-               NSAttributedString.Key.foregroundColor : UIColor.darkGray,
-               NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 20)
-           ]
+     enum Back: String {
+        case dismissCurrent
+        case popUpCurrent
+    }
+    func setupNavigationItems(backAction: Back, haveSharingBTN: Bool, haveBackBTN: Bool,title: String, view: UIView, sharingUrl:String) {
+        self.navigationItem.title = title
+        self.navigationController?.navigationBar.clipsToBounds = true
+        self.navigationController?.navigationBar.layer.maskedCorners = [.layerMinXMinYCorner,.layerMaxXMinYCorner]
+        self.navigationController?.navigationBar.titleTextAttributes = [
+            NSAttributedString.Key.foregroundColor : UIColor.darkGray,
+            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 20)
+        ]
+        if haveBackBTN{
+            let backItem = UIBarButtonItem(image: UIImage.init(named: "back2"), style: .done, target: self, action: Selector(backAction.rawValue))
+            backItem.tintColor = UIColor.gray
+           let leftPadding = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+           leftPadding.width = 18
+           navigationItem.leftBarButtonItems = [leftPadding, backItem]
+        }
+           if haveSharingBTN{
+               let SharingItem = UIBarButtonItem(image: UIImage.init(named: "sharingBTN"), style: .done, target: self, action: #selector(sharing))
+               SharingItem.tintColor = UIColor.gray
+               let rightPadding = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+               rightPadding.width = 18
+               navigationItem.rightBarButtonItems = [rightPadding, SharingItem]
+           }
+           
+       }
+       
+       @objc func popUpCurrent() {
+           navigationController?.popViewController(animated: true)
+       }
+       
+       @objc func dismissCurrent() {
+           dismiss(animated: true, completion: nil)
+       }
+       
+    @objc func sharing(url:String) {
+              let someText:String = "this recipe is from greatest recipe app"
+            let objectsToShare:URL = URL(string: "\(url)")!
+            let sharedObjects:[AnyObject] = [objectsToShare as AnyObject,someText as AnyObject]
+            let activityViewController = UIActivityViewController(activityItems : sharedObjects, applicationActivities: nil)
+            activityViewController.popoverPresentationController?.sourceView = self.view
+            activityViewController.excludedActivityTypes = [ UIActivity.ActivityType.airDrop, UIActivity.ActivityType.postToFacebook,UIActivity.ActivityType.postToTwitter,UIActivity.ActivityType.mail]
+            self.present(activityViewController, animated: true, completion: nil)
        }
 }
