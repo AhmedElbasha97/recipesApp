6ca7d098f22e85165f4b41a69c3e1a2e1e5d4f92
Ahmed elbasha <64855329+AhmedElbasha97@users.noreply.github.com>
2021-07-31 19:14:10 -0700

    set up recipe Details View

---
+++ recipesApp/view/RecipeDetails/recipeDetailsView.swift
@@ -0,0 +1,44 @@
+//
+//  recipeDetailsView.swift
+//  recipesApp
+//
+//  Created by ahmed elbasha on 31/07/2021.
+//  Copyright © 2021 ahmed elbasha. All rights reserved.
+//
+
+import UIKit
+class  recipDetailsView: UIView {
+    @IBOutlet weak var recipeDetalsImageView: UIImageView!
+    @IBOutlet weak var recipeDetailsTitleLabel: UILabel!
+    @IBOutlet weak var rwecipeDetailsInstrictionLabel: UILabel!
+    @IBOutlet weak var recipeDetailsButton: UIButton!
+        func setUp(view: UIView){
+         shapeTheBTN(BTN: recipeDetailsButton)
+         setUpUILabael(size: 30.0, isBold: true, Textolor: UIColor.black, label: recipeDetailsTitleLabel)
+         setUpUILabael(size: 20.0, isBold: false, Textolor: UIColor.black, label: rwecipeDetailsInstrictionLabel)
+            }
+    func ConfigureData(recipeImage:UIImage,recipeTitle:String,recipeIngridients:String){
+        self.recipeDetalsImageView.image=recipeImage
+        self.recipeDetailsTitleLabel.text = recipeTitle
+        self.rwecipeDetailsInstrictionLabel.text = recipeIngridients
+        
+    }
+}
+extension recipDetailsView{
+    private func shapeTheBTN(BTN: UIButton){
+        BTN.backgroundColor = .clear
+        BTN.layer.cornerRadius = BTN.frame.height / 2
+        BTN.layer.borderWidth = 4
+        BTN.layer.borderColor = UIColor.darkGray.cgColor
+        }
+    private func setUpUILabael(size: CGFloat, isBold: Bool, Textolor: UIColor,label:UILabel){
+          label.textColor = Textolor
+          if isBold {
+              label.font = UIFont.boldSystemFont(ofSize: size)
+          } else {
+            label.sizeToFit()
+            label.numberOfLines = 0
+            label.font = UIFont.systemFont(ofSize: size)
+          }
+      }
+}
